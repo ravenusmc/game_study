2148bcada23a58e46fb4174237e9b04dd55ffe7c
ravenusmc <mcuddy77@gmail.com>
2023-10-31 16:37:55 -0400

    finished the jumbotron on the landing page

---
+++ client/src/components/home/Jumbo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <div class="title-div">
+        <h1 class="display-4 Jumbo-heading">Game Study</h1>
+        <hr />
+        <p class="lead">A project examing game data</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbo",
+};
+</script>
+
+<style scoped>
+.jumbotron {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url("../../assets/images/game_image.jpg");
+  height: 550px;
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.title-div {
+  background-color: rgba(0, 0, 0, 0.8);
+  width: 40%;
+  padding: 40px;
+}
+
+.Jumbo-heading {
+  color: white;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+hr {
+  border-color: white;
+}
+
+.lead {
+  color: white;
+  text-align: center;
+  text-transform: capitalize;
+}
+</style>
+	
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,44 +1,21 @@
 <template>
   <div>
-    <div class="jumbotron">
-      <h1 class="display-4 Jumbo-heading">Game Study</h1>
-      <p class="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <hr class="my-4" />
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
-      </p>
-    </div>
+    <Jumbo/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import Jumbo from '@/components/home/Jumbo.vue'
 
 export default {
   name: "HomeView",
-  // components: {
-  //   HelloWorld
-  // }
+  components: {
+    Jumbo
+  }
 };
 </script>
 
 <style scoped>
-.jumbotron {
-  background-image: url("../assets/images/game_image.jpg");
-  height: 550px;
-  width: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
 
-.Jumbo-heading {
-  color: white;
-  text-transform: uppercase;
-}
 </style>
