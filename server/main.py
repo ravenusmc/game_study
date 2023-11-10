406e3724386c780b146085ef49e2779207f7b0a6
ravenusmc <mcuddy77@gmail.com>
2023-11-10 14:16:04 -0500

    working on method to get sentiment

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 #Importing files that I made: 
 from scraping import *
 from support import *
+from sentiment import *
 
 # instantiate the app
 app = Flask(__name__)
@@ -16,6 +17,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def fetchGameReviews():
     if request.method == 'POST':
         support = Support()
+        sentiment = Sentiment()
         post_data = request.get_json()
         game_title_lowercase = support.lower_case_all_letters(post_data)
         corrected_game_title = support.add_dash_to_game_titles(game_title_lowercase)
@@ -26,9 +28,8 @@ def fetchGameReviews():
         scores = scrape.get_scores_of_game()
         data_container.append(scores)
         critic_score_data, critic_quote_data = scrape.get_critic_scores_and_data()
+        sentiment.get_sentiment(critic_quote_data)
         user_score_data, user_quote_data = scrape.get_user_scores_and_data()
-        #print(critic_score_data)
-        print(critic_quote_data)
     return jsonify('5')
 
 if __name__ == '__main__':
--- server/sentiment.py
+++ server/sentiment.py
@@ -5,5 +5,9 @@ from textblob import TextBlob
 
 class Sentiment():
 
-    pass
-
+    def get_sentiment(self, sentiment_data):
+        count = 1
+        print(sentiment_data[count][1])
+        speech_text_ready_for_analysis = TextBlob(sentiment_data[count][1])
+        sentence_sentiment = speech_text_ready_for_analysis.sentiment[0]
+        print(sentence_sentiment)
