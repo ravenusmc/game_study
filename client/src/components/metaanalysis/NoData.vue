47472ecff0b0e160919490b4c580aad069d2cabd
ravenusmc <mcuddy77@gmail.com>
2023-11-14 16:45:30 -0500

    added more to no data component

--- client/src/components/metaanalysis/NoData.vue
+++ client/src/components/metaanalysis/NoData.vue
@@ -1,22 +1,51 @@
 <template>
 <div>
 	<section>
-		<h3>No Data</h3>
+		<div>
+			<h3 class='center'>Currently No Data</h3>
+			<p class='center'>Please Enter a Game Title Above</p>
+		</div>
+		<div class='pic-section'>
+			<div class='pic-one'></div>
+			<div class='pic-two'></div>
+		</div>
 	</section>
 </div>
 </template>
 
 <script>
 export default {
-	name: 'NoData',
-}
-
+  name: "NoData",
+};
 </script>
 
 <style scope>
-
 section {
-	height: 500px;
+  height: 700px;
 }
 
+.pic-section {
+	display: flex; 
+	flex-direction: row;
+}
+
+.pic-one {
+	background-image: url("../../assets/images/image_one.jpg");
+  height: 400px;
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+	margin: 30px;
+}
+
+.pic-two {
+	background-image: url("../../assets/images/image_two.jpg");
+  height: 400px;
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+	margin: 30px;
+}
 </style>
