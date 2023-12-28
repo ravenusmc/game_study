053336374576983df51e9e00853dfd122bbfe2f3
ravenusmc <mcuddy77@gmail.com>
2023-12-28 16:36:57 -0500

    adding ideas for game graphs

--- client/src/views/GameData.vue
+++ client/src/views/GameData.vue
@@ -5,6 +5,13 @@
 		</div>
 
 		What graphs to make?
+		-allow user to choose year and see best game, in a genere in that year 
+		-Graph of average genere ratings per year - avg RPG rating overtime 
+			-User should choose genere
+		-Sales, by publisher, by year 
+			-Need to see top 5 publishers probably 
+			-user clicks button to change to next year 
+		-Graph of user scores versus critic score for best game 
 
 
 	</div>
