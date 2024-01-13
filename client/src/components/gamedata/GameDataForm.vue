c944cb7d98f53b08834a945bd5a2080a48f25bb7
ravenusmc <mcuddy77@gmail.com>
2024-01-13 11:51:06 -0500

    working on form for game data page

---
+++ client/src/components/gamedata/GameDataForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form class="contact-form" @change="changeSelectedGraph">
+      <div class="form-group">
+        <label for="party">Select Graph:</label>
+        <select v-model="selectedGraph">
+          <option disabled value="">Please select one</option>
+          <option v-for="graph in Graphs" :key="graph" :value="graph">
+            {{ graph }}
+          </option>
+        </select>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+
+export default {
+  name: "AnalysisForm",
+  data() {
+    return {
+      Graphs: ["Box Plot","Histogram", "Scatter Plot"],
+      selectedGraph: "Box Plot",
+    };
+  },
+  methods: {
+    ...mapActions("meta", ["changeGraphs"]),
+    changeSelectedGraph() {
+      event.preventDefault();
+      const payload = {
+        graph: this.selectedGraph,
+      };
+      this.changeGraphs({ payload });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-group {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
--- client/src/views/GameData.vue
+++ client/src/views/GameData.vue
@@ -1,29 +1,36 @@
 <template>
   <div>
-		<div class='title-div'>
-			<h3 class='center'>Game Data Analysis</h3>
+    <div class="title-div">
+      <h3 class="center">Game Data Analysis</h3>
+    </div>
+
+		<div>
+			<GameDataForm />
 		</div>
 
-		What graphs to make?
+    <!-- What graphs to make?
 		-Sales, by publisher, by year 
 			-Need to see top 5 publishers probably 
 			-user clicks button to change to next year
 			-DONE  
 		-Graph of user scores versus critic score for best game 
-			-Should I do this?
-
-	</div>
+			-Should I do this? -->
+  </div>
 </template>
 
 <script>
+import GameDataForm from "@/components/gamedata/GameDataForm.vue";
+
 export default {
-	name: "GameData",
-}
+  name: "GameData",
+  components: {
+    GameDataForm,
+  },
+};
 </script>
 
 <style scoped>
-
 .title-div {
-	margin-top: 50px;
+  margin-top: 50px;
 }
 </style>
