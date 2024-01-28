23a3d13364504ef7d403088054da967c969ba71d
ravenusmc <mcuddy77@gmail.com>
2024-01-28 10:29:52 -0500

    working on nodata component

---
+++ client/src/components/gamedata/BlankData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section>
+      <div>
+        <h3 class='center'>Currently No Data</h3>
+        <p>Please Select a Year and Genre Above</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoData",
+};
+</script>
+
+<style scoped>
+section {
+	display: flex;
+	justify-content: center;
+	margin-top: 30px;
+}
+
+h3 {
+	text-transform: uppercase;
+}
+</style>
--- client/src/views/GameData.vue
+++ client/src/views/GameData.vue
@@ -1,41 +1,43 @@
 <template>
   <div>
-    <div class="title-div">
-      <h3 class="center">Game Data Analysis</h3>
-    </div>
-		<div>
-			<GameDataForm />
-		</div>
-		<section>
-			<NoData v-if="!dataReceived"  />
-			<Results v-if="dataReceived" />
-		</section>
-    <p>{{ dataReceived }}</p>
+    <section>
+      <div class="title-div">
+        <h2 class="center">Game Data Analysis</h2>
+      </div>
+      <div>
+        <GameDataForm />
+      </div>
+      <div>
+        <BlankData v-if="!dataReceived" />
+        <Results v-if="dataReceived" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import GameDataForm from "@/components/gamedata/GameDataForm.vue";
-import NoData from "@/components/gamedata/NoData.vue";
+import BlankData from "@/components/gamedata/BlankData.vue";
 import Results from "@/components/gamedata/Results.vue";
 
 export default {
   name: "GameData",
   components: {
-		GameDataForm,
-		NoData,
-		Results,
+    GameDataForm,
+    BlankData,
+    Results,
   },
   computed: {
-		...mapGetters("csv", 
-		["dataReceived"])
-	},
+    ...mapGetters("csv", ["dataReceived"]),
+  },
 };
 </script>
 
 <style scoped>
+
 .title-div {
   margin-top: 50px;
+	text-transform: uppercase;
 }
 </style>
